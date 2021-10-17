b7969cb4716c562b8787464f17c6aea44471837a
Ronit <ronitbhati.786@gmail.com>
2021-10-17 11:38:25 +0530

    intial commit

---
+++ main/views.py
@@ -0,0 +1,24 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from main.models import Contact
+from django.contrib import messages
+
+def home(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        messege = request.POST['messege']
+
+        if len(name) < 3 or len(email) < 8 or len(phone) < 10 or len(messege) < 5:
+            messages.error(request, "Please fill the form correctly!")
+
+        else:
+            contact = Contact(name=name, email=email, phone=phone, messege=messege)
+            contact.save()
+            messages.success(request, "Your form has been submitted!")
+
+    return render(request, 'index.html')
+
+def browse(request):
+    return render(request, 'browse.html')
